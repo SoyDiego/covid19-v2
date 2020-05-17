67aac5944e300b54930b79132e3641850086851a
SoyDiego <diegofranchina@gmail.com>
2020-05-18 01:51:38 +0200

    Remove npm bootstrap and added bootstrap locally

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App.jsx';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import './Components/css/bootstrap.min.css'
 
 ReactDOM.render(
   <React.StrictMode>
